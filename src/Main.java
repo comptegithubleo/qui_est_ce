2b04748deb7184f34cd3aaef0372231d596d111a
projetprogl2 <vert.stylo@laposte.net>
2022-01-31 19:16:25 +0100

    setup 2 JavaFX scenes

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import com.fasterxml.jackson.databind.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("app/Menu.fxml"));
+			Scene scene = new Scene(root, 300, 300);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		System.out.println("main launched");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		launch(args);
+	}
+}
