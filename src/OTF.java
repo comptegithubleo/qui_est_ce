0255cc389d7fc52d4e7571df24ef915268fc5f68
fuflam <74704275+fuflam@users.noreply.github.com>
2022-02-02 19:43:02 +0100

    update : trouvé comment désérialiser un json dans une classe

--- src/OTF.java
+++ src/OTF.java
@@ -1,19 +1,27 @@
+
+
 public class OTF {
 
     private String nom;
     private String age;
-    private int test;
 
     public OTF(){}
 
-    public OTF(String nom, String age, int test){
+    public OTF(String nom, String age){
         this.nom = nom;
         this.age = age;
-        this.test = test;
     }
 
     public String toString(){
-        return "je suis " + this.nom + " j'ai " + this.age + "je lance le test : " + this.test; 
+        return "je suis " + this.nom + " j'ai " + this.age + " ans, je lance le test"; 
     }
     
+    public void setnom(String nom){
+        this.nom = nom;
+    }
+
+    public void setage(String age){
+        this.age = age;
+    }
+
 }
