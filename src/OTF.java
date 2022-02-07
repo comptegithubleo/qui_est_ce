c8bc2e925a228b1688fd406cef00e4f228175bfe
fuflam <74704275+fuflam@users.noreply.github.com>
2022-02-07 15:54:41 +0100

    small changes such as source attribute becoming src

--- src/OTF.java
+++ src/OTF.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class OTF {
 
     private String id;
-    private String source;
+    private String src;
     private ArrayList<String> attribute;
 
     public OTF(){}
 
-    /*public OTF(String id, String source, ArrayList<String> attribute){
+    /*public OTF(String id, String src, ArrayList<String> attribute){
         this.id = id;
-        this.source = source;
+        this.src = src;
         this.attribute = attribute;
     } commented but usable to manually create an instance*/
 
@@ -25,8 +25,8 @@ public class OTF {
         this.id = id;
     }
 
-    public void setsource(String source){
-        this.source = source;
+    public void setsrc(String src){
+        this.src = src;
     }
 
     public void setattribute(ArrayList<String> attribute){
