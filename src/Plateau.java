122d4e05c228d8c9226f1fc7a5e836bf173d1bf9
fuflam <74704275+fuflam@users.noreply.github.com>
2022-02-14 14:17:43 +0100

    Merge branch 'main' of https://github.com/projetprogl2/qui_est_ce

---
+++ src/Plateau.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+import java.util.random.*;
+
+public class Plateau {
+
+    private ArrayList<OTF> plateau;
+    private OTF ITF;
+    
+    public Plateau(){
+
+    }
+
+    public void chooseITF(){
+        Random rand = new Random();
+        this.ITF = plateau.get(rand.nextInt(plateau.size()));
+    }
+    
+
+    
+}
