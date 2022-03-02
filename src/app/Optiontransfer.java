9686991b1eb4b6db009b245dc67254db67a840b2
fuflam <74704275+fuflam@users.noreply.github.com>
2022-03-02 14:52:50 +0100

    New class used to transer the difficulty selected from option to main menu

---
+++ src/app/Optiontransfer.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Optiontransfer {
+
+    private String option;
+
+    public Optiontransfer(){}
+
+    public String getOption(){
+        return this.option;
+    }
+
+    public void setOption(String s){
+        this.option = s;
+    }
+}
